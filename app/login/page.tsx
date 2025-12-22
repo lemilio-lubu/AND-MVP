90210b56161e7647072cfe99d8f40baa92553737
Emilio Jacome <emilio.jacome@epn.edu.ec>
2025-12-21 20:08:46 -0500

    feat: login page skeleton

---
+++ app/login/page.tsx
@@ -0,0 +1,10 @@
+export default function LoginPage() {
+  return (
+    <main style={{ padding: 40 }}>
+      <h2>Iniciar sesión</h2>
+      <input placeholder="Email" />
+      <input placeholder="Password" type="password" />
+      <button>Entrar</button>
+    </main>
+  );
+}
