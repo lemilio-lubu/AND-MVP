d2af7737f35690fa4ede36f8b894237cc0017b0e
Emilio Jacome <emilio.jacome@epn.edu.ec>
2026-02-08 20:55:38 -0500

    feat:implementación de tag manager de meta

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Montserrat, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/app/components/theme-provider";
 import { UserProvider } from "@/lib/context/UserContext";
+import Script from "next/script";
 
 const montserrat = Montserrat({
   variable: "--font-montserrat",
@@ -26,7 +27,29 @@ export default function RootLayout({
 }) {
   return (
     <html lang="es" className="scroll-smooth" data-scroll-behavior="smooth" suppressHydrationWarning>
+      <head>
+        {/* Google Tag Manager */}
+        <Script id="google-tag-manager" strategy="afterInteractive">
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-KLQPHWLZ');
+          `}
+        </Script>
+      </head>
       <body className={`${montserrat.variable} ${geistMono.variable} antialiased bg-[var(--background)] text-[var(--foreground)] font-sans transition-colors duration-300`}>
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-KLQPHWLZ"
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          ></iframe>
+        </noscript>
+        
         <ThemeProvider
           attribute="class"
           defaultTheme="dark"
