3b043b16e196f1fb3ac7633f1090346f84fa0284
Emilio Jacome <emilio.jacome@epn.edu.ec>
2026-02-10 10:30:56 -0500

    chore: cambio de gtm

--- app/layout.tsx
+++ app/layout.tsx
@@ -35,7 +35,7 @@ export default function RootLayout({
             new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
             j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
             'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-KLQPHWLZ');
+            })(window,document,'script','dataLayer','GTM-MLXMSS2N');
           `}
         </Script>
       </head>
@@ -43,7 +43,7 @@ export default function RootLayout({
         {/* Google Tag Manager (noscript) */}
         <noscript>
           <iframe
-            src="https://www.googletagmanager.com/ns.html?id=GTM-KLQPHWLZ"
+            src="https://www.googletagmanager.com/ns.html?id=GTM-MLXMSS2N"
             height="0"
             width="0"
             style={{ display: 'none', visibility: 'hidden' }}
